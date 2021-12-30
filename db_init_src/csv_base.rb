7911abe6d1d741ef6023d581390d695f5bfe5eee
dogwood008 <dogwood008+github@gmail.com>
2021-12-30 16:06:32 +0900

    :sparkles: Add seconds

--- db_init_src/csv_base.rb
+++ db_init_src/csv_base.rb
@@ -5,13 +5,23 @@ class CsvBase
     @file_path = file_path
   end
 
+  # each_line前に実施することがあればオーバライドして記載する
+  def preprocess(file_path)
+    pass
+  end
+
+  # each_line後に実施することがあればオーバライドして記載する
+  def postprocess(file_path)
+    pass
+  end
+
   def each_line
-    open(@file_path, 'r') do |f|
-      CSV.foreach(@file_path,
-          headers: header?, encoding: encoding) do |row|
-        yield(parse(row))
-      end
+    file_path = preprocess(@file_path) || @file_path
+    CSV.foreach(file_path,
+        headers: header?, encoding: encoding) do |row|
+      yield(parse(row))
     end
+    postprocess(file_path)
   end
 
   def parse(row)
@@ -31,6 +41,26 @@ class CsvBase
   # @params [CSV::Row] row
   def price(row)
     raise NotImplementedError
+  end
+
+  protected
+
+  # 前場
+  # @returns [Array<Array<Integer, Integer>>] [[9, 0], ..., [11, 30]]
+  def zenba_time
+    @zenba_time ||= [*(9..10).to_a.product((0..59).to_a), *[11].product((0..30).to_a)].freeze
+  end
+
+  # 後場
+  # @returns [Array<Array<Integer, Integer>>] [[12, 30], ..., [15, 0]]
+  def goba_time
+    @goba_time ||= [*[12].product((30..59).to_a), *(13..14).to_a.product((0..59).to_a), [15, 0]].freeze
+  end
+
+  # ザラ場
+  # @returns [Array<Array<Integer, Integer>>] [[9, 0], ..., [15, 0]]
+  def zaraba_time
+    @zaraba_time ||= [*zenba_time, *goba_time].freeze
   end
 
   private
--- db_init_src/sbi.rb
+++ db_init_src/sbi.rb
@@ -1,5 +1,6 @@
 require './csv_base'
 require 'time'
+require 'tempfile'
 
 class Sbi < CsvBase
   def header?
@@ -7,7 +8,37 @@ class Sbi < CsvBase
   end
   
   def encoding
+    'utf-8'
+  end
+
+  def original_encoding
     'sjis:utf-8'
+  end
+
+  def preprocess(file_path)
+    csv = CSV.read(file_path, encoding: original_encoding).reverse  # SBIのCSV歩み値は、時間降順になっているため
+
+    zaraba_time.each do |pseudo_current_hm_arr|
+      pseudo_current_hm = pseudo_current_hm_arr.map { |hm| sprintf('%02d', hm) }.join('')
+      time_column_index = 1
+      rows_to_updates = csv.find_all { |row| row[time_column_index] == pseudo_current_hm }
+      rows_size = rows_to_updates.empty? ? 1 : rows_to_updates.size
+      rows_to_updates.each.with_index do |row, i|
+        pseudo_seconds = sprintf('%02d', i * 60 / rows_size)
+        row[time_column_index] = "#{pseudo_current_hm}#{pseudo_seconds}"  # 秒を按分
+      end
+    end
+    f = Tempfile.open
+    CSV.open(f.path, 'wb') do |csv_fw|
+      csv.reverse.each do |row|  # 再度降順に戻す
+        csv_fw << row
+      end
+    end
+    f
+  end
+
+  def postprocess(file_path)
+    file_path.close
   end
 
   def parse(row)
@@ -28,7 +59,7 @@ class Sbi < CsvBase
 
   # @params [CSV::Row] row
   def time(row)
-    Time.strptime(row['時間'], '%H%M').strftime('%H:%M:00+0900')
+    Time.strptime(row['時間'], '%H%M%S').strftime('%H:%M:%S+0900')
   end
 
   def volume(row)
