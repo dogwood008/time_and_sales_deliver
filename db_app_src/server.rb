3a9f61247b742c3746fece324eeb9d7554c3e96d
dogwood008 <dogwood008+github@gmail.com>
2022-01-05 23:45:30 +0900

    :sparkles: Give args from URL parameters

--- db_app_src/server.rb
+++ db_app_src/server.rb
@@ -6,8 +6,11 @@ set :bind, '0.0.0.0'
 
 app = App.new
 
+# e.g.) /7974/2021-09-09T14:59:30
 get '/:code/:datetime' do
-  app.exec(7974, '2021-09-09 14:59:30').to_json
+  stock_code = params['code']
+  datetime = params['datetime']
+  app.exec(stock_code, datetime).to_json
 end
 
 get '/exit' do
